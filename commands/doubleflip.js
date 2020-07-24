3f3b5d1e6a6ca231729c8289626f898144ed29be
SmintOverflow <smintoverflow@gmail.com>
2020-07-24 17:48:16 +0530

    Create doubleflip.js

---
+++ commands/doubleflip.js
@@ -0,0 +1,7 @@
+
+const Discord = require('discord.js')
+const Subiex = require('../bot.js')
+
+Subiex.registerCommand('doubleflip', 'default', (message) => {
+  return '┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻'
+}, [], 'Double table flip!', '[]')
