f301068edfa678b7ccd3fa1e8ce28c23ba65f494
Smint <ssg.jss2018@gmail.com>
2020-08-15 11:15:04 +0530

    changed currency api to finnhub

--- commands/crypto.js
+++ commands/crypto.js
@@ -23,17 +23,19 @@
     ------------------------------------------------------------------------
 
 */
-const Subiex = require('../bot.js')
+const Confax = require('../bot.js')
 const request = require('request')
-const Util = require('util')
+const Discord = require('discord.js')
+const getSymbolFromCurrency = require('../modules/currency')
+const { queryFinnhub } = require('../modules/finnhub')
 
-Subiex.registerCommand('crypto', 'default', (message, bot) => {
+Confax.registerCommand('crypto', 'default', (message, bot) => {
   //  Init variables
-  let symbols = (message.content.split(' ')).map(function (x) { return x.toUpperCase() })
-  let fromSymbol = (symbols.shift())
+  const symbols = (message.content.split(' ')).map(function (x) { return x.toUpperCase() })
+  const fromSymbol = (symbols.shift()).replace(/`/ig, '')
   if (symbols.length === 0) symbols.push('USD')
-  CrytpoComparePrice(fromSymbol, symbols, message)
-}, ['crypt', 'price'], 'Get latest crypto exchange price in USD, or in other cryptos.', '<crypto-currency ticker> (crypto-currency ticker )')
+  CryptoComparePrice(fromSymbol, symbols, message)
+}, ['crypt', 'price'], 'Get latest crypto exchange price in USD, or in other cryptos.', '<crypto-currency ticker>')
 
 /**
  * Return the exchange rate for one crypto currency in terms of other currencies.
@@ -41,35 +43,32 @@ Subiex.registerCommand('crypto', 'default', (message, bot) => {
  * @param  {string[]} tsyms
  * @param  {string} messge
  */
-function CrytpoComparePrice (fsym, tsyms, message) {
+function CryptoComparePrice (fsym, tsyms, message) {
   //  The API call string.
-  let cryptoComparePrice = 'https://min-api.cryptocompare.com/data/price?fsym=' + fsym + '&tsyms=' + tsyms
+  const cryptoComparePrice = 'https://min-api.cryptocompare.com/data/price?fsym=' + fsym + '&tsyms=' + tsyms
+
   request(cryptoComparePrice, function (error, response, body) {
-    if (error) message.channel.send('Not a valid crypto currency, try BTC or ETH.')
-    let finalMessage = '**~~------~~** __Current exchange rates for 1 ' + fsym + '__ **~~------~~**\n```'
+    if (error) {
+      return queryFinnhub(message, fsym)
+    }
+    const embed = new Discord.MessageEmbed().setTitle('cryptocompare.com')
+      .setDescription('```MarkDown\n#_Exchange rates for 1 ' + fsym + '```')
+      .setURL('https://www.cryptocompare.com/api/')
+      .setColor(586901)
+      .setTimestamp()
     try {
-      let responseBody = JSON.parse(body)
+      const responseBody = JSON.parse(body)
       for (let sym = 0; sym < tsyms.length; sym++) {
         let price = responseBody[tsyms[sym]]
-        if (price === undefined) price = '¯\\_(ツ)_/¯' // Not valid crypto currency ¯\_(ツ)_/¯
-        if (tsyms[sym] === 'USD') price = '$' + price
-        finalMessage += Util.format('%s %s\n', pad('.............', tsyms[sym], false), price)
+        if (price === undefined) {
+          return queryFinnhub(message, fsym)
+        } else if (getSymbolFromCurrency(tsyms[sym]) !== undefined) price = ' ' + getSymbolFromCurrency(tsyms[sym]) + ' ' + parseFloat(price).toLocaleString('en-IN', { maximumSignificantDigits: 10, minimumFractionDigits: 2 })
+        else price = parseFloat(price).toLocaleString('en-IN', { maximumSignificantDigits: 10, minimumFractionDigits: 2 })
+        embed.addField('*  ' + tsyms[sym], '```js\n' + price + '```', true)
       }
-      message.channel.send(finalMessage + '```')
+      message.channel.send('**' + fsym + '**', {embed})
     } catch (error) {
-      message.channel.send('<:doggo:328259712963313665>' + ' Not a valid crypto-currency, try BTC or ETH.')
+      return queryFinnhub(message, fsym)
     }
   })
-}
-
-/**
- * Pad a string on the left or right for better alignment.
- * @param  {string} pad
- * @param  {string} str
- * @param  {boolean} padLeft
- */
-function pad (pad, str, padLeft) {
-  if (typeof str === 'undefined') return pad
-  if (padLeft) return (pad + str).slice(-pad.length)
-  else return (str + pad).substring(0, pad.length)
 }
