4e2e92a2a73771e1dfc7293c61f0a534648ac6aa
Smint <ssg.jss2018@gmail.com>
2020-08-20 21:58:40 +0530

    added duckduckgo cmd

---
+++ commands/duckduckgo.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const Subiex = require('../bot.js')
+
+Subiex.registerCommand('duckduckgo', 'default', (message, bot) => {
+    if (message.content.length == 0) {
+        message.channel.send({
+            embed: {
+                color: 0xfc1846,
+                description: '**' + 'Error, empty argument' + '**',
+            }
+        });
+        return;
+    }
+    message.channel.send(":zap: **DuckDuckGo Brrr** :zap: ")
+    message.channel.send('http://duckduckgo.com/?q=' + encodeURIComponent(message))
+}, ['privategoogle', 'ddg', 'duckduckgobrrr'], 'Generate a duckduckgo search (basically google without trackers) link', '<expression>')
